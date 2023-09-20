2f62538734ba74d694c2b613111f41ac4a3ed022
Paulo Daniel Gonzalez <paulo.gonzalez@hginsights.com>
2023-09-20 10:01:59 -0500

    Add third commit to branch

--- lib/deps_example_target.ex
+++ lib/deps_example_target.ex
@@ -15,6 +15,7 @@ defmodule DepsExampleTarget do
   def hello do
     IO.puts("First change, first commit")
     IO.puts("Second change, second commit")
+    IO.puts("Third change, third commit, this commit will not be referenced")
     :world
   end
 end
